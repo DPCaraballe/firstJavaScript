4df2338b38617745e36f3c251fb145af5d43414a
DPCaraballe <84527568+DPCaraballe@users.noreply.github.com>
2021-06-09 16:21:28 +0200

    Add files via upload

---
+++ opdrachtjavascript/index.js
@@ -0,0 +1,16 @@
+
+console.log('Hello Winc Academy');
+
+let name = 'Daniel'
+let age = 'fortyfour' 
+
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(88 + 78);
+console.log(456 - 68);
+console.log(88 * 88);
+console.log(98 / 5);
+console.log(5 % 2);
+console.log(typeof age);
+
+
